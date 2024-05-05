2cebd61e2188b1ba1041fa0ed1c9b593b1cd2d0d
MuhammAd Azam <107695431+azam-amir@users.noreply.github.com>
2024-05-05 15:00:33 +0500

    [sunday] working on dom in javascript

--- 3-javascript/2-Dom-Introduction/app.js
+++ 3-javascript/2-Dom-Introduction/app.js
@@ -5,10 +5,16 @@ console.log(document); //it will return you all html
 const taskBtn = document.querySelector("#task-button");
 
 taskBtn.addEventListener("click", function (event) {
+  // console.log(event.target, "event.target");
   event.preventDefault(); //apni default functionality rukdien
-  console.log("clicked!");
+  // console.log("clicked!");
+
+  //agar text Task List ke brabar hai to change hojae warna wapsi Task List hojae
 
   const selectCardTitle = document.querySelector(".card-title");
+
+  // console.log(selectCardTitle, "selectCardTitle");
+
   if (selectCardTitle.innerText === "Task List") {
     selectCardTitle.innerText = "It is Changed";
     selectCardTitle.style.color = "red";
@@ -20,8 +26,11 @@ taskBtn.addEventListener("click", function (event) {
 
 let valOfDom;
 valOfDom = document;
+// console.log(valOfDom);
 valOfDom = document.all;
+// console.log(valOfDom);
 valOfDom = document.body;
+// console.log(valOfDom);
 valOfDom = document.head;
 valOfDom = document.doctype;
 valOfDom = document.URL;
@@ -33,5 +42,16 @@ valOfDom = document.links; // a tags
 valOfDom = document.links[0].className; // a ki clases ajayengi
 valOfDom = document.images;
 valOfDom = document.scripts;
+valOfDom = document.scripts[0];
+valOfDom = document.scripts[0].getAttribute("src");
+
+const cardTitle = document.querySelector(".card-title");
+
+cardTitle.style.color = "yellow";
+cardTitle.style.background = "red";
+cardTitle.style.padding = "20px";
+cardTitle.style.marginBottom = "20px";
+cardTitle.style.display = "none";
+cardTitle.style.display = "block";
 
 console.log(valOfDom, "val of dom");
