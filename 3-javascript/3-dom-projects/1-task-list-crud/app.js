7ed4363fb3f147ed3c9528f475b55abe6619d342
MuhammAd Azam <107695431+azam-amir@users.noreply.github.com>
2024-05-25 14:55:43 +0500

    [saturday] working on task list project

--- 3-javascript/3-dom-projects/1-task-list-crud/app.js
+++ 3-javascript/3-dom-projects/1-task-list-crud/app.js
@@ -1,11 +1,14 @@
 const taskForm = document.querySelector("#task-form");
 const taskInput = document.querySelector("#task");
+const collection = document.querySelector(".collection");
+const clearTaskBtn = document.querySelector(".clear-tasks");
 
 taskForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
   const inputValue = taskInput.value;
   //agar ye input value mujood na ho
+
   if (!inputValue) {
     alert("please fill the input field");
     return;
@@ -29,8 +32,6 @@ create this type of element in dom
     <i class="fa fa-remove"></i>
   </a>`;
 
-  const collection = document.querySelector(".collection");
-
   collection.appendChild(liElement);
 
   taskInput.value = "";
@@ -50,3 +51,24 @@ appendChild returns the appended node.
 append does not return anything.
 
 */
+
+clearTaskBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  if (confirm("Are you sure ?")) {
+    collection.innerHTML = "";
+  }
+});
+
+collection.addEventListener("click", function (event) {
+  event.preventDefault();
+  const currentElement = event.target;
+
+  if (currentElement.className === "fa fa-remove") {
+    if (confirm("Are you sure ?")) {
+      currentElement.parentElement.parentElement.remove();
+    }
+  }
+});
+
+//localstorage vs session storage
