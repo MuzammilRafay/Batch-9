2801d03aab8ce971f57f34986a579b5e00056131
MuhammAd Azam <107695431+azam-amir@users.noreply.github.com>
2024-05-26 15:05:35 +0500

    [sunday] learn localstorage/sessionStorage

--- 3-javascript/3-dom-projects/1-task-list-crud/app.js
+++ 3-javascript/3-dom-projects/1-task-list-crud/app.js
@@ -37,6 +37,8 @@ create this type of element in dom
   taskInput.value = "";
 
   //   console.log(liElement);
+
+  saveAllTasksOnLocalStorage();
 });
 
 /*
@@ -65,6 +67,7 @@ collection.addEventListener("click", function (event) {
   const currentElement = event.target;
 
   if (currentElement.className === "fa fa-remove") {
+    // if (currentElement.classList.includes("fa fa-remove")) {
     if (confirm("Are you sure ?")) {
       currentElement.parentElement.parentElement.remove();
     }
@@ -72,3 +75,46 @@ collection.addEventListener("click", function (event) {
 });
 
 //localstorage vs session storage
+
+// session storage will expire on closing the chrome tab
+
+// sessionStorage.setItem("something",true);
+// sessionStorage.getItem("something");
+
+//localstorage
+
+// session storage will not expire on closing the chrome tab
+
+// localStorage.setItem("something",true);
+// localStorage.getItem("something");
+
+//we cannot save object/array on localstorage
+
+// so there is a way to save it
+
+// JSON.stringify() (during the localStorage.setItem)
+// JSON.parse() (during the localStorage.getItem)
+
+// localStorage.setItem(
+//   "tasks",
+//   JSON.stringify(["taskOne", "TaskTwo", "TaskThree"])
+// );
+
+// const getTasks = JSON.parse(localStorage.getItem("tasks"));
+
+// console.log(getTasks, "getTasks");
+
+function saveAllTasksOnLocalStorage() {
+  const selectAllCollectionItems =
+    document.querySelectorAll(".collection-item");
+  // console.log(selectAllCollectionItems, "selectAllCollectionItems");
+
+  let tasks = [];
+  selectAllCollectionItems.forEach(function (singleCollectionItem) {
+    tasks.push(singleCollectionItem.innerText);
+  });
+
+  console.log(tasks, "tasks");
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
