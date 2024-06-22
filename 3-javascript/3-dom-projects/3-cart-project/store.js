e81c3ebb602084f5178719c5a25be186a34e3aa8
MuzammilRafay <muzammil.rafay@gmail.com>
2024-06-22 14:56:35 +0500

    [saturday] working on add to cart project

--- 3-javascript/3-dom-projects/3-cart-project/store.js
+++ 3-javascript/3-dom-projects/3-cart-project/store.js
@@ -30,7 +30,7 @@ cusomShopContainer.addEventListener("click", function (e) {
             <span class="cart-column">
               $ <span class="cart-price-item-item">${cartItemPrice}</span>
             </span>
-            <div class="cart-quantity cart-column">
+            <div class="cart-quantity cart-column" style="flex-grow:1;">
               <input class="cart-quantity-input" type="number" value="1" />
               <button class="btn btn-danger btn-remove" type="button">
                 REMOVE
@@ -39,5 +39,53 @@ cusomShopContainer.addEventListener("click", function (e) {
 
     // console.log(divElement, "divElement");
     cartItems.append(divElement);
+
+    calculateTotal();
+  }
+});
+
+function calculateTotal() {
+  const selectAllCartRows = document.querySelectorAll(".cart-items .cart-row");
+
+  let total = 0;
+  selectAllCartRows.forEach(function (singleCartRow) {
+    const cartPrice = singleCartRow.querySelector(
+      ".cart-price-item-item"
+    ).innerText;
+
+    const cartQty = singleCartRow.querySelector(".cart-quantity-input").value;
+
+    total += parseFloat(cartPrice) * cartQty;
+    // total = total + parseFloat(cartPrice) * cartQty;
+  });
+
+  const getTotalElement = document.querySelector(".cart-total-price");
+
+  // getTotalElement.innerText = ` $   ${total.toFixed(2)}`;
+
+  getTotalElement.innerText = `$ ${total.toFixed(2)}`;
+  // console.log(total.toFixed(2), "total");
+}
+
+cartItems.addEventListener("change", function (event) {
+  const currentElement = event.target;
+
+  if (currentElement.classList.contains("cart-quantity-input")) {
+    //input ki value zero se choti hai yaaa brabar hai zero kee use brabar kardo 1 ke
+    if (currentElement.value <= 0) {
+      currentElement.value = 1;
+    }
+    calculateTotal();
+  }
+});
+
+cartItems.addEventListener("click", function (event) {
+  const currentElement = event.target;
+
+  if (currentElement.classList.contains("btn-remove")) {
+    if (confirm("Are you sure ?")) {
+      currentElement.parentElement.parentElement.remove();
+      calculateTotal();
+    }
   }
 });
