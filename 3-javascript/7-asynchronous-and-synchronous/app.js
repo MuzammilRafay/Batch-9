7483ce16b2eab73c1c8399b1edc70d22f8816e55
MuzammilRafay <muzammil.rafay@gmail.com>
2024-07-13 14:57:30 +0500

    [saturday] how we can convert our normal to asyncronous code

--- 3-javascript/7-asynchronous-and-synchronous/app.js
+++ 3-javascript/7-asynchronous-and-synchronous/app.js
@@ -18,6 +18,8 @@ setTimeout(function () {
 console.log("react js");
 console.log("react native");
 
+//html css, react js, react native, js
+
 //is js is asyncronous by default ?
 //answer is no
 
@@ -48,3 +50,86 @@ console.log("react native");
 // 1-Callback/functions (Web Api Callback)
 // 2-Promise
 // 3-Async Await
+
+//1- Callback example
+
+setTimeout(function () {
+  console.log("asdasdasd");
+}, 0);
+
+//2-Promise
+
+//Creating the promise function
+const doSomething = new Promise(function (resolve, reject) {
+  const success = 0;
+
+  if (success == 1) {
+    resolve("your code is resolved");
+  }
+
+  if (success == 0) {
+    reject("your code is rejected");
+  }
+});
+
+//Using the promise function
+
+//then = resolve wala code
+//catch = reject wala code
+
+doSomething
+  .then(function (data) {
+    console.log(data, "[resolve message parameter]");
+  })
+  .catch(function (data) {
+    console.log(data, "[reject message parameter]");
+  });
+
+//fetch method is used for getting the api response
+//fetch method is promise
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(function (resolveParameter) {
+    return resolveParameter.json(); //
+    console.log(resolveParameter, "[fetch method resolve]");
+  })
+  .then(function (afterReturnResponse) {
+    console.log(afterReturnResponse, "[afterReturnResponse]");
+  })
+
+  // .then(async function (response) {
+  //   const convertedJsonData = await response.json();
+  //   console.log(convertedJsonData, "convertedJsonData");
+  // })
+  .catch(function (rejectParameter) {
+    console.log(rejectParameter, "[reject parameter reject]");
+  });
+
+//3. Async Await
+// you can handle multiple promises from async await method
+
+const getTodosData = function () {
+  //you are returning the promise
+  return (
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then(function (resolve) {
+        return resolve.json();
+      })
+      //chaining in promise
+      .then(function (resolve) {
+        return resolve;
+      })
+      .catch(function (reject) {
+        console.log(reject);
+      })
+  );
+};
+
+const doSomethingAsyncMethod = async function () {
+  const dataOne = await getTodosData(); //  ka matlab wait karo jab tak promise resolve nai hojata
+  const dataTwo = await getTodosData();
+  const dataThree = await getTodosData();
+  console.log(dataOne, "dataOne async method");
+  console.log(dataTwo, "dataTwo async method");
+  console.log(dataThree, "dataThree async method");
+};
+doSomethingAsyncMethod();
