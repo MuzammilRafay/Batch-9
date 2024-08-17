4cc4a72a55f5414ebf75b5d5ae4f6fbe866f7a7d
MuhammAd Azam <107695431+azam-amir@users.noreply.github.com>
2024-08-17 14:56:21 +0500

    [saturdy] learn react js re-render states & create component & its props

--- 4-reactjs/1-introduction-react-project/src/App.js
+++ 4-reactjs/1-introduction-react-project/src/App.js
@@ -1,20 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
-
+import DisplayCount from "./components/DisplayCount/DisplayCount";
 function App() {
-  // let count = 0;
-  const [count, setCount] = useState(0);
+  let countTwo = 0;
+  const [count, setCount] = useState(0); // [getValue,setValue]
 
   const incrementHandler = () => {
-    // count = count + 1;
+    // countTwo = countTwo + 1;
     setCount(count + 1);
     console.log(count, "clicked");
+    // console.log(countTwo, "clicked");
   };
   return (
     <div className="App">
       <header className="App-header">
-        <p>Count: {count}</p>
+        <DisplayCount count={count} countTwo={countTwo} />
         <button onClick={incrementHandler}>Increment</button>
       </header>
     </div>
---
+++ 4-reactjs/1-introduction-react-project/src/components/DisplayCount/DisplayCount.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DisplayCount = (props) => {
+  console.log(props, "props");
+  return (
+    <>
+      <p>Count: {props.count}</p>
+      <p>Count Two: {props.countTwo}</p>
+    </>
+  );
+};
+
+export default DisplayCount;
