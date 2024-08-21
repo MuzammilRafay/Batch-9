b8155c723805f2b0dceff6532a821835cb2bbb0d
MuhammAd Azam <107695431+azam-amir@users.noreply.github.com>
2024-08-21 21:58:23 +0500

    [wednesday] learn loop in react js and useeffect & show or hide condition in reat js

--- 4-reactjs/1-introduction-react-project/src/App.js
+++ 4-reactjs/1-introduction-react-project/src/App.js
@@ -1,10 +1,33 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DisplayCount from "./components/DisplayCount/DisplayCount";
+
+const tasks = [
+  {
+    name: "task one",
+  },
+  {
+    name: "task one",
+  },
+  {
+    name: "task one",
+  },
+  {
+    name: "task one",
+  },
+  {
+    name: "task one",
+  },
+  {
+    name: "task one",
+  },
+];
+
 function App() {
   let countTwo = 0;
   const [count, setCount] = useState(0); // [getValue,setValue]
+  const [isShowCountBtn, setIsShowCountBtn] = useState(true);
 
   const incrementHandler = () => {
     // countTwo = countTwo + 1;
@@ -12,11 +35,33 @@ function App() {
     console.log(count, "clicked");
     // console.log(countTwo, "clicked");
   };
+
+  useEffect(() => {
+    //it will run on first render of the component
+    console.log("first render");
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <DisplayCount count={count} countTwo={countTwo} />
-        <button onClick={incrementHandler}>Increment</button>
+        <br />
+        {isShowCountBtn && (
+          <button onClick={incrementHandler}>Increment</button>
+        )}
+        <br />
+
+        <button
+          onClick={() => {
+            //true ko false kardega or false ko true kardega
+            setIsShowCountBtn(!isShowCountBtn);
+          }}
+        >
+          Show / Hide
+        </button>
+
+        {/* {tasks.map((singleTask) => {
+          return <p>{singleTask.name}</p>;
+        })} */}
       </header>
     </div>
   );
--- 4-reactjs/1-introduction-react-project/src/components/DisplayCount/DisplayCount.jsx
+++ 4-reactjs/1-introduction-react-project/src/components/DisplayCount/DisplayCount.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+
+
 const DisplayCount = (props) => {
   console.log(props, "props");
   return (
     <>
       <p>Count: {props.count}</p>
       <p>Count Two: {props.countTwo}</p>
+
+      
     </>
   );
 };
